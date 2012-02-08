3ed5f84a74cd291f5291e85bc68004efaea56991
Jamie English <jamienglish@gmail.com>
2012-02-08 21:41:46 +0000

    Specking from_xml. Have it using ATTRIBUTE nodes so far

--- lib/product.rb
+++ lib/product.rb
@@ -6,6 +6,15 @@ class Product
     @attributes = []
   end
 
+  def self.from_xml(xml)
+    product = Product.new
+
+    product_node = Nokogiri::XML(xml)
+    product.add_attributes_from_xml product_node
+
+    product
+  end
+
   def add_attribute(name, value)
     unless has_attribute? name
       @attributes << Attribute.new(name, value)
@@ -15,7 +24,7 @@ class Product
   def attribute_value(name)
     if self.respond_to? name.downcase.to_sym, :include_privates
       self.send name.downcase.to_sym
-    else
+    elsif has_attribute?(name)
       @attributes.find {|attr| attr.name == name }.value
     end
   end
@@ -29,14 +38,14 @@ class Product
   end
 
   def add_attributes_from_xml(xml)
-    @attributes += xml.css('ATTRIBUTE').map {|att| Attribute.from_xml(att) }
+    @attributes += xml.css('ATTRIBUTE').map {|att| Attribute.from_xml(att) }.compact
   end
 
   private
 
   def multiple(attribute_name)
     [ @attributes.find {|attr| attr.name == attribute_name }.value,
-      attribute_value("2nd #{attribute_name}") ].join ','
+      attribute_value("2nd #{attribute_name}") ].compact.join(',')
   end
 
   def stone
--- spec/product_spec.rb
+++ spec/product_spec.rb
@@ -28,6 +28,10 @@ describe Product do
           <ATT-DESC>Collection</ATT-DESC>
           <ATT-VALUE>Cruise Sport</ATT-VALUE>
         </ATTRIBUTE>
+        <ATTRIBUTE>
+          <ATT-DESC></ATT-DESC>
+          <ATT-VALUE></ATT-VALUE>
+        </ATTRIBUTE>
       </ITEM>
     XML
 
@@ -40,16 +44,75 @@ describe Product do
     watch.attribute_value('Collection').must_equal 'Cruise Sport'
   end
 
-  it "joins attributes with multiple values" do
-    ring = Product.new
-    ring.add_attribute 'Stone',     'Diamond'
-    ring.add_attribute '2nd Stone', 'Ruby'
+  describe "attributes that can have multiple values" do
+    it "joins attributes with multiple values" do
+      ring = Product.new
+      ring.add_attribute 'Stone',     'Diamond'
+      ring.add_attribute '2nd Stone', 'Ruby'
 
-    ring.attribute_value('Stone').must_equal 'Diamond,Ruby'
+      ring.attribute_value('Stone').must_equal 'Diamond,Ruby'
 
-    ring.add_attribute 'Material',     'Silver'
-    ring.add_attribute '2nd Material', 'Gold'
+      ring.add_attribute 'Material',     'Silver'
+      ring.add_attribute '2nd Material', 'Gold'
 
-    ring.attribute_value('Material').must_equal 'Silver,Gold'
+      ring.attribute_value('Material').must_equal 'Silver,Gold'
+    end
+  end
+
+  describe :from_xml do
+    let(:xml) do
+      <<-XML
+        <ITEM>
+          <ATTR>A</ATTR> <STKNO>0101044</STKNO> <MENU>1</MENU> <SUBMENU>1</SUBMENU>
+          <SUPPLCODE>1001</SUPPLCODE> <SUPPLSHORT>UDC</SUPPLSHORT>
+          <SUPPLREF>ML99/25</SUPPLREF> <DESC>18ctYG SS Dia RO/XO</DESC>
+          <QTY>1</QTY> <PRICE>0000795.00</PRICE> <REASON></REASON>
+          <CATEGORY>1</CATEGORY> <WEBDESC>18ct Yellow Gold Diamond Twist Ring</WEBDESC>
+          <NOTEPAD><![CDATA[The round brilliant diamond is set in a soft curved mount creating a rub over setting, completing this stylish twist ring. ]]> </NOTEPAD>
+          <ATTRIBUTE>
+            <ATT-TYPE>BR</ATT-TYPE> <ATT-DESC>Brand</ATT-DESC> <ATT-FORMAT>L</ATT-FORMAT> <ATT-CODE>01</ATT-CODE> <ATT-VALUE>Other</ATT-VALUE>
+          </ATTRIBUTE>
+          <ATTRIBUTE>
+            <ATT-TYPE>C3</ATT-TYPE> <ATT-DESC>Stone Cut</ATT-DESC> <ATT-FORMAT>L</ATT-FORMAT> <ATT-CODE></ATT-CODE> <ATT-VALUE></ATT-VALUE>
+          </ATTRIBUTE>
+          <ATTRIBUTE>
+            <ATT-TYPE>C7</ATT-TYPE> <ATT-DESC>Stone</ATT-DESC> <ATT-FORMAT>T</ATT-FORMAT> <ATT-CODE></ATT-CODE> <ATT-VALUE>Diamond</ATT-VALUE>
+          </ATTRIBUTE>
+          <ATTRIBUTE>
+            <ATT-TYPE>C8</ATT-TYPE> <ATT-DESC>Stone Setting</ATT-DESC> <ATT-FORMAT>L</ATT-FORMAT> <ATT-CODE></ATT-CODE> <ATT-VALUE></ATT-VALUE>
+          </ATTRIBUTE>
+          <ATTRIBUTE>
+            <ATT-TYPE>G1</ATT-TYPE> <ATT-DESC>Gender</ATT-DESC> <ATT-FORMAT>L</ATT-FORMAT> <ATT-CODE>01</ATT-CODE> <ATT-VALUE>Ladies</ATT-VALUE>
+          </ATTRIBUTE>
+          <ATTRIBUTE>
+            <ATT-TYPE>M1</ATT-TYPE> <ATT-DESC>Material</ATT-DESC> <ATT-FORMAT>T</ATT-FORMAT> <ATT-CODE></ATT-CODE> <ATT-VALUE>Yellow Gold</ATT-VALUE>
+          </ATTRIBUTE>
+          <ATTRIBUTE>
+            <ATT-TYPE>M2</ATT-TYPE> <ATT-DESC>2nd Material</ATT-DESC> <ATT-FORMAT>T</ATT-FORMAT> <ATT-CODE></ATT-CODE> <ATT-VALUE></ATT-VALUE>
+          </ATTRIBUTE>
+          <ATTRIBUTE>
+            <ATT-TYPE>R2</ATT-TYPE> <ATT-DESC>Ring Type 2</ATT-DESC> <ATT-FORMAT>L</ATT-FORMAT> <ATT-CODE>02</ATT-CODE> <ATT-VALUE>Single Stone</ATT-VALUE>
+          </ATTRIBUTE>
+          <ATTRIBUTE>
+            <ATT-TYPE>R3</ATT-TYPE> <ATT-DESC>Ring Type 3</ATT-DESC> <ATT-FORMAT>L</ATT-FORMAT> <ATT-CODE></ATT-CODE> <ATT-VALUE></ATT-VALUE>
+          </ATTRIBUTE>
+          <ATTRIBUTE>
+            <ATT-TYPE>RT</ATT-TYPE> <ATT-DESC>Ring Type</ATT-DESC> <ATT-FORMAT>L</ATT-FORMAT> <ATT-CODE>02</ATT-CODE> <ATT-VALUE>Engagement</ATT-VALUE>
+          </ATTRIBUTE>
+          <ATTRIBUTE>
+            <ATT-TYPE>S2</ATT-TYPE> <ATT-DESC>2nd Stone</ATT-DESC> <ATT-FORMAT>T</ATT-FORMAT> <ATT-CODE></ATT-CODE> <ATT-VALUE></ATT-VALUE>
+          </ATTRIBUTE>
+          <ATTRIBUTE>
+            <ATT-TYPE>TW</ATT-TYPE> <ATT-DESC>Total Weight</ATT-DESC> <ATT-FORMAT>V</ATT-FORMAT> <ATT-CODE></ATT-CODE> <ATT-VALUE>0.00</ATT-VALUE>
+          </ATTRIBUTE>
+        </ITEM>
+      XML
+    end
+
+    it "loads the ATTRIBUTE nodes to attributes" do
+      product = Product.from_xml xml
+      product.attribute_value('Gender').must_equal 'Ladies'
+      product.attribute_value('Material').must_equal 'Yellow Gold'
+    end
   end
 end
